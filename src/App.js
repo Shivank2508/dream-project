ebfe909c4377f9235a0aba1a3f43608f93948976
ShivankTyagi <tyagishivank1212@gmail.com>
2023-01-13 12:51:17 +0530

    Add existing project files to Git

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  let sum = 5;
+  return (
+    <div className="App">
+      <h1>shivank tyagi</h1>
+      <p>{sum}</p>
+    </div>
+  );
+}
+
+export default App;
