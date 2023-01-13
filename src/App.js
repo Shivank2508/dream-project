f33f670d3ef5e48811fb458965b88f8d7450dc82
ShivankTyagi <tyagishivank1212@gmail.com>
2023-01-13 13:14:17 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-
-import './App.css';
+import React, { Fragment } from 'react';
 
 function App() {
-  let sum = 5;
   return (
-    <div className="App">
-      <h1>shivank tyagi</h1>
-      <p>{sum}</p>
-    </div>
+    <Fragment>
+      <h1 data-ns-type="project-name">Social Media App</h1>
+      <p data-ns-type="project-description">If I had to recommend only one app for you to add to your portfolio, it would be a social media app. Twitter, Facebook,
+        and Instagram are quite sophisticated, and include an ever-growing number of features to keep users engaged</p>
+    </Fragment>
   );
 }
 
